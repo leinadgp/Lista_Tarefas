9ec012bea5081fc94e8ad5c47f6126435e164e46
leinadgp <leinadgp@gmail.com>
2025-07-28 23:24:29 -0300

    Lista de tarefas

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
+import { Container, TodoList, Input, Button, ListItem,Lixeira, Check,Paragrafo } from "./styles.js"
+
+
+
+
+function App() {
+  // const listaTarefas = [
+  // { id: uuidv4(), conteudo: "Levar a Loma para passear"}, 
+  // { id: uuidv4(), conteudo: "Comprara Maça" },
+  // { id: uuidv4(), conteudo: "Terminar aulas de React DEV Club" }, 
+  // { id: uuidv4(), conteudo: "nova tarega" },
+  // { id: uuidv4(), conteudo: "teste nova taregaaaaa" }
+  // ],
+  const [imputTask, setImputTask] = useState([])
+  const [list, setList] = useState([])
+
+
+
+  function imputMudou(event) {
+    setImputTask(event.target.value)
+    return [...list, { id: uuidv4(), conteudo: event.target.value }]
+  }
+  function clickButton() {
+    if (imputTask.length === 0) {
+      alert("Digite uma tarefa")
+    }else{
+
+    setList([...list, { id: uuidv4(), conteudo: imputTask, finished: false }])
+    }
+  }
+  function finalizarTarefa(id) {
+    const newList = list.map(item => {
+      if (item.id === id) {
+        return { ...item, finished: !item.finished }
+      }
+      return item
+    })
+    setList(newList)
+  }
+  function deletarTarefa(id) {
+    const newList = list.filter(item => item.id !== id)
+    setList(newList)
+  }
+
+  return (
+
+    <Container>
+      <TodoList>
+        <Input onChange={imputMudou} type="text" placeholder="Digite o que tenho para fazer" />
+        <Button onClick={clickButton}>Adicionar</Button>
+        <ul>
+          
+         
+          { 
+            list.length === 0 ? <Paragrafo>Você não tem tarefas cadastradas</Paragrafo> :
+           
+            list.map(item => (
+              <ListItem $isFinished={item.finished}  key={item.id}>
+                <Check onClick={ () => finalizarTarefa(item.id)} />
+                <li> {item.conteudo} </li>
+                <Lixeira onClick={ () => deletarTarefa(item.id)}  />
+              </ListItem>
+            ))
+          }
+        </ul>
+      </TodoList>
+
+    </Container>
+
+
+
+  )
+}
+
+export default App
---
+++ src/styles.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+import { FaTrash, FaRegThumbsUp } from "react-icons/fa6";
+
+export const Container = styled.div`
+        background: linear-gradient(90deg, #383838 0% , #000000 81.25% ) ;
+        width: 100vw;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+`
+export const Paragrafo = styled.p`
+        text-align: center;
+        color: #ff0000;
+        font-size: 25px;
+`
+
+export const TodoList = styled.div`
+        
+        background-color: #FFFF;
+        padding: 30px 20px;
+        border-radius: 5px;
+        max-width: 80%;
+
+   ul{
+        padding: 0;
+        margin-top: 60px;
+       
+    }
+   
+`
+export const Input = styled.input`
+        
+        border: 2px solid rgba(209, 211, 212, 0.4);
+        border-radius: 5px;
+        height: 40px;
+        width: 342px;
+        margin-right: 40px;
+        padding: 0 10px;
+
+`
+export const Button = styled.button`
+        
+        background: #8052ec;
+        border-radius: 5px;
+        width: 130px;
+        color: #fff;
+        border: none;
+        height: 40px;
+        font-size: 17px;
+        line-height: 2px;
+        font-weight: 900;
+        cursor: pointer;
+         &:hover {
+        opacity: 0.8;
+         }
+        &:active {
+        opacity: 0.4;
+        }
+`
+export const ListItem = styled.div`
+        
+        background: ${props => props.$isFinished ? "#e8ff8b" : "#e4e4e4"};
+        
+        
+
+        box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
+        padding: 15px;
+        display: flex;
+        height: 60px;
+        width: 500px;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 30px;
+        gap: 20px;
+     li{
+         list-style-type: none;
+     }   
+`
+
+export const Lixeira = styled(FaTrash)`
+        cursor: pointer;
+`
+export const Check = styled(FaRegThumbsUp)`
+        cursor: pointer;
+`
